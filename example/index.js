991db2e7bb776742ad48fd414362415c127f8810
david <nopbyte>
2016-10-18 22:16:25 +0200

    fix example, flatten the user id from now on... a simple string also fix issue with promise

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@ var IdmCore = require('../index');
 
 
 idmcore = new IdmCore(conf);
-var token = "2493143689353019";
+var token = "6328602477442473";
 var action = "create";
 var entity_type = "/Sensor";
 var entity_id = "3";
@@ -15,6 +15,6 @@ var data = {
 var prom = idmcore.actionPromisse(token,action , entity_type , entity_id, data);
 prom.then(function(data){
   console.log('data from api: '+JSON.stringify(data));
-}, function(error){
+}).catch(function(error){
   console.log('something went wrong: '+error);
 })
