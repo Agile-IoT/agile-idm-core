63b2cb52a29a3148ade1c7d2496f4078d3ce8f6d
david <nopbyte>
2016-11-03 16:52:48 +0100

    add entity to group, not thoroughly  tested yet tests for cases when adding unexistent entity to a group or adding existing entity to unexistent group

--- tests/sqlite3-storage-test.js
+++ tests/sqlite3-storage-test.js
@@ -310,6 +310,7 @@ describe('Sqlite3Storage', function () {
     });
 
   });
+
   describe('#Create amd Read Group()', function () {
     //called after each test to delete the database
     afterEach(function () {
@@ -347,6 +348,7 @@ describe('Sqlite3Storage', function () {
       var storeConf = {
         "dbName": dbName
       };
+
       var storage = new Sqlite3Storage();
       storage.init(storeConf, function () {
         storage.readGroupPromise("unexistent-stuff", "user")
@@ -361,4 +363,63 @@ describe('Sqlite3Storage', function () {
     });
 
   });
+
+  describe('#Add and list entities in a Group', function () {
+    //called after each test to delete the database
+    afterEach(function () {
+
+      if (fs.existsSync(dbName))
+        fs.unlinkSync(dbName);
+
+    });
+
+    it('should reject with 404 error when attempting to place an unexistent entity in a group that exists', function (done) {
+      var storeConf = {
+        "dbName": dbName
+      };
+      var owner = "1";
+      var group_name = "mygroup";
+      var storage = new Sqlite3Storage();
+      storage.init(storeConf, function () {
+        storage.createGroupPromise(group_name, owner)
+        .then(function(group){
+              return storage.AddEntityToGroupByIdsPromise(group.id,"unexistent-stuff", "user")
+        }).then(function (result) {
+            throw Error("should not give results");
+          }, function reject(error) {
+            if (error.statusCode == 404) {
+              done();
+            }
+          });
+      });
+    });
+
+    it('should reject with 404 error when attempting to place an exitent entity in a group is not there', function (done) {
+      var storeConf = {
+        "dbName": dbName
+      };
+      var storage = new Sqlite3Storage();
+      var owner = "1";
+      var entity_id = "2";
+      var entity_type = "user";
+      var data = {
+        "name": "string",
+        "token": "123"
+      };
+      storage.init(storeConf, function () {
+        storage.createEntityPromise(entity_id, entity_type, owner, data)
+        .then(function(entity){
+              return storage.AddEntityToGroupByIdsPromise("unexistent-group", entity.id, entity.type)
+        }).then(function (result) {
+            throw Error("should not give results");
+          }, function reject(error) {
+            if (error.statusCode == 404) {
+              done();
+            }
+          });
+      });
+    });
+
+  });
+
 });
