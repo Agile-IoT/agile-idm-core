118bcd838e97cbd718764debc276c70db5dd28bf
david <nopbyte@users.noreply.github.com>
2017-07-28 14:59:50 +0200

    create two configs and abstract configurations a bit... pass main config for locks down to pdp

---
+++ tests/standard-conf.js
@@ -0,0 +1,201 @@
+module.exports = {
+  "storage": {
+    "dbName": "./database"
+  },
+  "upfront_storage":{
+      module_name: "agile-upfront-leveldb",
+      type: "external",
+      dbName: "./pap-database",
+      collection: "policies"
+  },
+  "policies": {
+    "create_entity_policy": [
+      // actions of an actor are not restricted a priori
+      {
+        op: "write"
+      },
+      {
+        op: "read"
+      }
+    ],
+    "top_level_policy": {
+      flows: [
+        // all properties can be read by everyone
+        {
+          op: "read"
+        },
+        // all properties can only be changed by the owner of the entity
+        {
+          op: "write",
+          locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
+            lock: "isOwner"
+          }]
+        },
+        {
+          op: "write",
+          locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
+            lock: "attrEq",
+            args: ["role", "admin"]
+          }]
+        }
+      ],
+      //specify what should happen if the policy does not comply
+      actions: {
+        "read": [{
+          action: "delete"
+        }]
+      }
+    },
+    "attribute_level_policies": {
+      "user": {
+        "password": [
+          // the property can only be read by the user itself
+          {
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          }
+          // the property can be set by the user itself and
+          , {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
+        "role": [
+          // can be read by everyone
+          {
+            op: "read"
+          },
+          // can only be changed by users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ]
+      },
+      "sensor": {
+        "credentials": [
+          // the property can only be read by the user itself
+          {
+            op: "read",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // the property can be set by the user itself and
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          // by all users with role admin
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ]
+      }
+
+    }
+  },
+  "schema-validation": [{
+    "id": "/sensor",
+    "type": "object",
+    "additionalProperties": false,
+    "properties": {
+      "name": {
+        "type": "string"
+      },
+      "token": {
+        "type": "string"
+      }
+    },
+    "required": ["name"]
+  }, {
+    "id": "/user",
+    "type": "object",
+    "additionalProperties": false,
+    "properties": {
+      "user_name": {
+        "type": "string"
+      },
+      "auth_type": {
+        "type": "string"
+      },
+      "password": {
+        "type": "string"
+      },
+      "role": {
+        "type": "string"
+      },
+      "credentials": {
+        "type": "object",
+        "additionalProperties": true,
+        "properties": {
+          "dropbox": {
+            "type": "string"
+          }
+        }
+      }
+    },
+    "required": ["user_name", "auth_type"]
+  },{
+    "id": "/other",
+    "type": "object",
+    "additionalProperties": false,
+    "properties": {
+      "name": {
+        "type": "string"
+      },
+      "token": {
+        "type": "string"
+      }
+    },
+    "required": ["name"]
+  }]
+};
